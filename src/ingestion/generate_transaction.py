3fe2bb6c369d0bccde177714c09a8afa74b019d4
Ernest Rouyrre <erouyrre@gmail.com>
2026-04-08 12:11:35 +0200

    V1.0.0

--- src/ingestion/generate_transaction.py
+++ src/ingestion/generate_transaction.py
@@ -1,5 +1,6 @@
 '''
-Génère des transactions selon la structure suivante :
+Generate a list of clients and transactions with random data.
+The tables are as follows:
 Table CLIENTS
 ─────────────────────────────
 client_id     : str (UUID)  PK
@@ -25,7 +26,91 @@ is_fraud        : bool
 __author__ = "Ernest Rouyrre"
 
 
+import uuid
+import random
+import pandas as pd
+from faker import Faker
 from datetime import datetime
-from uuid import uuid4
+from enum import Enum
+from pathlib import Path
 
-import faker as fk
+
+fake = Faker()
+
+class CardType(Enum):
+    CREDIT = "CREDIT"
+    DEBIT = "DEBIT"
+    PREPAID = "PREPAID"
+
+class TransactionStatus(Enum):
+    APPROVED = "APPROVED"
+    DECLINED = "DECLINED"
+    PENDING = "PENDING"
+
+def generate_clients(n: int) -> list[dict]:
+    '''
+    Generate a list of clients with random data.
+    '''
+    list_clients = []
+    for _ in range(n):
+        client = {
+            "client_id": str(uuid.uuid4()),
+            "firstname": fake.first_name(),
+            "lastname": fake.last_name(),
+            "address": fake.address(),
+            "account_balance": random.uniform(0, 2000),
+        }
+        list_clients.append(client)
+    return list_clients
+    
+def get_different_client(clients, emetteur_id):
+    '''
+    Return a different client from the list of clients.
+    '''
+    client_id = random.choice(clients)["client_id"]
+    while client_id == emetteur_id:
+        client_id = random.choice(clients)["client_id"]
+    return client_id
+
+def generate_transactions(n: int, clients: list[dict]) -> list[dict]:
+    '''
+    Generate a list of transactions with random data.
+    '''
+    list_transactions = []
+    for _ in range(n):
+        emeteur_id = random.choice(clients)["client_id"]
+        destinataire_id = get_different_client(clients, emeteur_id)
+        is_fraud = random.random() < 0.02
+        transaction = {
+            "transaction_id": str(uuid.uuid4()),
+            "timestamp": fake.date_time_between(start_date="-1y", end_date="now"),
+            "emetteur_id": emeteur_id,
+            "destinataire_id": destinataire_id,
+            "card_type": random.choice(list(CardType)).value,
+            "location": fake.address(),
+            "status": random.choice(list(TransactionStatus)).value,
+            "is_fraud": is_fraud,
+            "montant": random.uniform(0, 10000) if is_fraud else random.uniform(0, 1000)
+        }
+        list_transactions.append(transaction)
+    return list_transactions
+
+def save_to_bronze(clients: list[dict], transactions: list[dict]):
+    '''
+    Save the clients and transactions to a bronze table as a parquet file.
+    '''
+    CURRENT_DIR = Path(__file__).parent
+    SAVE_FOLDER = CURRENT_DIR.parent.parent.joinpath("data", "bronze")
+    SAVE_FOLDER.mkdir(parents=True, exist_ok=True)
+    CLIENTS_FILE = SAVE_FOLDER.joinpath("clients.parquet")
+    TRANSACTIONS_FILE = SAVE_FOLDER.joinpath("transactions.parquet")
+    df_clients = pd.DataFrame(clients)
+    df_transactions = pd.DataFrame(transactions)
+    df_clients.to_parquet(CLIENTS_FILE, index=False)
+    df_transactions.to_parquet(TRANSACTIONS_FILE, index=False)
+
+
+if __name__ == "__main__":
+    clients = generate_clients(1000)
+    transactions = generate_transactions(10000, clients)
+    save_to_bronze(clients, transactions)
